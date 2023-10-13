f687a2769b32423f2411a6ccb5355062d5675fe7
Dmytro Sayko <saykodmytro@icloud.com>
2023-10-13 10:48:48 +0300

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,21 @@
+import { TitleComponent } from './TitleComponent';
+import { Product } from './Product';
+import { productsData } from 'Data/productsData';
+
 export const App = () => {
-  return <div>React Dimon template</div>;
+  return (
+    <div>
+      <TitleComponent />
+
+      {productsData.map(({ id, title, price, discount }) => {
+        return (
+          <Product key={id} title={title} price={price} discount={discount} />
+        );
+      })}
+
+      {/* <Product title="Tacos With Lime" price="7.99" discount={1.4} />
+      <Product title="Tacos With Lemon" price="4.99" discount={2.6} />
+      <Product title="Tacos L" price="2.99" discount={null} /> */}
+    </div>
+  );
 };
---
+++ src/components/Product.jsx
@@ -0,0 +1,20 @@
+import { productsData } from 'Data/productsData';
+
+export const Product = ({ title, price, discount = null }) => (
+  <div>
+    <img
+      src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+      alt="Tacos With Lime"
+      width="640"
+    />
+    <h2>{title}</h2>
+    {/* {discount && <h3>Discount: {discount}$</h3>} */}
+    {discount ? (
+      <h3>Discount: {discount}$</h3>
+    ) : (
+      <p>Sorry , but discount on this product has expired!</p>
+    )}
+    <p>Price: {price}$</p>
+    <button type="button">Add to cart</button>
+  </div>
+);
